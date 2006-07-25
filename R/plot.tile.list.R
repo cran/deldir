45f0511c379cdec52c3d14afa20bb2b75f0eb2bd
Rolf Turner <rolf@math.unb.ca>
2006-07-25 01:04:13 +0000

    version 0.0-5

--- R/plot.tile.list.R
+++ R/plot.tile.list.R
@@ -1,4 +1,5 @@
-plot.tile.list <- function(x,verbose=TRUE,...)
+plot.tile.list <- function (x, verbose = FALSE, close=FALSE, pch=1,
+                            polycol=NA, showpoints=TRUE, ...)
 {
 object <- x
 if(!inherits(object,"tile.list"))
@@ -8,22 +9,32 @@ x.all <- unlist(lapply(object,function(w){c(w$pt[1],w$x)}))
 y.all <- unlist(lapply(object,function(w){c(w$pt[2],w$y)}))
 x.pts <- unlist(lapply(object,function(w){w$pt[1]}))
 y.pts <- unlist(lapply(object,function(w){w$pt[2]}))
-plot(x.pts,y.pts,xlim=range(x.all),ylim=range(y.all),xlab='x',ylab='y',pch=21)
+rx <- range(x.all)
+ry <- range(y.all)
+plot(x.all, y.all, type = "n", asp=1, xlab = "x", ylab = "y")
+polycol <- rep(polycol,length=length(object))
+
 for(i in 1:n) {
-	x  <- object[[i]]$x
-	y  <- object[[i]]$y
-	bp <- object[[i]]$bp
-	ni <- length(x)
-	nim1 <- ni - 1
-	for(j in 1:nim1) {
-		do.it <- (ni==2) | (!(bp[j] & bp[j+1]))
-		if(do.it) segments(x[j],y[j],x[j+1],y[j+1])
+	inner <- !any(object[[i]]$bp)
+	if(close | inner)
+		polygon(object[[i]],col=polycol[i])
+	else {
+		x  <- object[[i]]$x
+		y  <- object[[i]]$y
+		bp <- object[[i]]$bp
+		ni <- length(x)
+		for(j in 1:ni) {
+			jnext <- if(j < ni) j+1 else 1
+                	do.it <- mid.in(x[c(j,jnext)],y[c(j,jnext)],rx,ry)
+                	if (do.it)
+                  		segments(x[j], y[j], x[jnext], y[jnext])
+		}
 	}
-	do.it <- (!(bp[ni] & bp[1]))
-	if(do.it) segments(x[ni],y[ni],x[1],y[1])
-        if(verbose & i < n) readline('Go? ')
-        points(object[[i]]$pt[1],object[[i]]$pt[2],pch=3)
+        if(verbose & showpoints)
+		points(object[[i]]$pt[1],object[[i]]$pt[2],pch=pch)
+	if(verbose & i < n) readline('Go? ')
 }
+if(showpoints) points(x.pts,y.pts,pch=pch)
+
 invisible()
 }
-
--- R/tile.list.R
+++ R/tile.list.R
@@ -2,6 +2,8 @@ tile.list <- function(object)
 {
 if(!inherits(object,"deldir"))
         stop("Argument \"object\" is not of class deldir.\n")
+rw  <- object$rw
+c0  <- 0.5*(rw[c(1,3)] + rw[c(2,4)])
 ddd <- object$dirsgs
 sss <- object$summary
 npts <- nrow(sss)
@@ -15,13 +17,25 @@ for(i in 1:npts) {
         m  <- rbind(m1,m2)
         pt <- sss[i,1:2]
         theta <- atan2(m[,2]-pt[2],m[,1]-pt[1])
+	theta <- ifelse(theta>0,theta,theta+2*pi)
         theta.0 <- sort(unique(theta))
-	m.save <- m
         m <- m[match(theta.0,theta),]
-        rslt[[i]] <- list(pt=sss[i,1:2],x=m[,1],y=m[,2],
-                          bp=as.logical(m[,3]))
+	bp <- as.logical(m[,3])
+        x  <- m[,1]
+        y  <- m[,2]
+        xynew <- list(x=x,y=y,bp=bp,joff=0) 
+        ni    <- length(bp)
+	if(ni==2) xynew <- get.cnrpt(1,xynew,c0)
+        else for(j in 1:ni) {
+		jprev <- if(j > 1) j-1 else ni
+		jnext <- if (j < ni) j + 1 else 1
+		if (!bp[jprev] & bp[j] & bp[jnext]) {
+			xynew <- get.cnrpt(j, xynew,c0)
+                }
+        }
+        rslt[[i]] <- list(pt = pt, x = xynew$x, y = xynew$y,
+                          bp = xynew$bp)
 }
 class(rslt) <- "tile.list"
 rslt
 }
-
