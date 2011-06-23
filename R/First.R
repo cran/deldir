22bae09a42b1c25b5cc02f93626ec5aee059c88b
Rolf Turner <r.turner@auckland.ac.nz>
2011-06-23 00:00:00 +0000

    version 0.0-14

--- R/First.R
+++ R/First.R
@@ -1,8 +1,9 @@
 .First.lib <- function(lib,pkg) {
 	library.dynam("deldir", pkg, lib)
 	ver <- read.dcf(file.path(lib, pkg, "DESCRIPTION"), "Version")
-        cat(paste(pkg, ver, "\n"))
-	cat(paste("\n     Please note: The process for determining duplicated",
+        packageStartupMessage(paste(pkg, ver))
+	packageStartupMessage(
+            paste("\n     Please note: The process for determining duplicated",
                   "points\n     has changed from that used in version 0.0-9",
-                  "(and previously).\n\n"))
+                  "(and previously).\n"))
 }
