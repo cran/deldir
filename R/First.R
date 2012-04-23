913a228d9f5b588e2e6ecaaf616914568c9c5d7c
Rolf Turner <r.turner@auckland.ac.nz>
2012-04-23 00:00:00 +0000

    version 0.0-19

--- R/First.R
+++ R/First.R
@@ -3,7 +3,11 @@
 	ver <- read.dcf(file.path(lib, pkg, "DESCRIPTION"), "Version")
         packageStartupMessage(paste(pkg, ver))
 	packageStartupMessage(
-            paste("\n     Please note: The process for determining duplicated",
+            paste( "\n     PLEASE NOTE:  The components \"delsgs\" and \"summary\"",
+                  "of the", "\n     object returned by deldir() are now",
+                  "DATA FRAMES rather than","\n     matrices (as they were prior",
+                  "to release 0.0-18).",    "\n     See help(\"deldir\").\n",
+                  "\n     PLEASE NOTE: The process for determining duplicated",
                   "points\n     has changed from that used in version 0.0-9",
                   "(and previously).\n"))
 }
