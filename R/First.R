f06630fe5d9bb42287a7e56e1ac04ad9714a3bb4
Rolf Turner <r.turner@auckland.ac.nz>
2012-09-08 05:59:20 +0000

    version 0.0-20

--- R/First.R
+++ R/First.R
@@ -2,12 +2,4 @@
 	library.dynam("deldir", pkg, lib)
 	ver <- read.dcf(file.path(lib, pkg, "DESCRIPTION"), "Version")
         packageStartupMessage(paste(pkg, ver))
-	packageStartupMessage(
-            paste( "\n     PLEASE NOTE:  The components \"delsgs\" and \"summary\"",
-                  "of the", "\n     object returned by deldir() are now",
-                  "DATA FRAMES rather than","\n     matrices (as they were prior",
-                  "to release 0.0-18).",    "\n     See help(\"deldir\").\n",
-                  "\n     PLEASE NOTE: The process for determining duplicated",
-                  "points\n     has changed from that used in version 0.0-9",
-                  "(and previously).\n"))
 }
